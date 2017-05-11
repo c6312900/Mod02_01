6ce941320fd4c4d615c548815a9b191113da61b7
c6312900 <c6312900@gmail.com>
2017-05-11 17:52:37 +0800

    1060511

---
+++ Mod02_01/Models/CheckValidYear.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod02_01.Models
+{
+  public  class CheckValidYear : ValidationAttribute
+    {
+        public CheckValidYear()
+        {
+            ErrorMessage = "The earliest opera is Daphne ,1598.by Corsi Peri, and Rinuccini";
+        }
+        public override bool IsValid(object value)
+        {
+            int year = (int)value;
+            if (year < 1598)
+            {
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+    }
+}
